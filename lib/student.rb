436bc030dfde963195f722b85cb2ee65c456552e
Leslie G <lesliegonzalez@me.com>
2020-08-26 19:47:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,14 @@
 class Student < User
   
+  @@all = []
+  
 def initialize(knowledge = [])
   @knowledge = knowledge
+  @@all << self
+end
+
+def learn(knowledge)
+  Student << knowledge
 end
 
 end
